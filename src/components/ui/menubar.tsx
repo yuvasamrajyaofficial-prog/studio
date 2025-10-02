0ac4c2b850e9f99aa84f1517547c68ef209c1652
Yuva Samrajya <yuvasamrajyaofficial@gmail.com>
2025-10-02 15:24:12 +0000

    what is -45? fix it completely

--- src/components/ui/menubar.tsx
+++ src/components/ui/menubar.tsx
@@ -107,14 +107,13 @@ const MenubarContent = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof MenubarPrimitive.Content>
 >(
   (
-    { className, align = "start", alignOffset = -4, sideOffset = 8, ...props },
+    { className, align = "start", sideOffset = 8, ...props },
     ref
   ) => (
     <MenubarPrimitive.Portal>
       <MenubarPrimitive.Content
         ref={ref}
         align={align}
-        alignOffset={alignOffset}
         sideOffset={sideOffset}
         className={cn(
           "z-50 min-w-[12rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
