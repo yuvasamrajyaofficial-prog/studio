d70d87588fab783b650eefde64b2e739083c588f
Yuva Samrajya <yuvasamrajyaofficial@gmail.com>
2025-11-21 15:05:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2676120085-8f624",
+  "appId": "1:117407088021:web:3a008422bf398cfdd14a9c",
+  "apiKey": "AIzaSyAV3usHqCl9vVT-8DpBho1YPr-kVjphI4o",
+  "authDomain": "studio-2676120085-8f624.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "117407088021"
+};
