71680e31cc456d313f744e003e0228c8d7a08e43
Yuva Samrajya <yuvasamrajyaofficial@gmail.com>
2025-11-21 14:15:09 +0000

    remove this and shanka (_for element <PadmaIcon>_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@
 import { Header } from "@/components/layout/header";
 import { Footer } from "@/components/layout/footer";
 import { SudharshanaChakraIcon } from "@/components/icons/sudharshana-chakra";
-import { ShankhaIcon } from "@/components/icons/shankha";
-import { PadmaIcon } from "@/components/icons/padma";
 
 export default function HomePage() {
 
@@ -15,16 +13,8 @@ export default function HomePage() {
         <Header />
 
         <main className="flex-1 container mx-auto p-8 flex flex-col items-center justify-center">
-            <div className="w-full max-w-5xl mx-auto flex items-center justify-around">
-                <div className="flex-1 flex justify-center items-center">
-                    <ShankhaIcon className="w-32 h-32 text-primary/80" />
-                </div>
-                <div className="flex-1 flex justify-center items-center">
-                    <SudharshanaChakraIcon className="w-64 h-64 animate-spin-slow" />
-                </div>
-                <div className="flex-1 flex justify-center items-center">
-                    <PadmaIcon className="w-32 h-32 text-primary/80" />
-                </div>
+            <div className="w-full max-w-5xl mx-auto flex items-center justify-center">
+                <SudharshanaChakraIcon className="w-64 h-64 animate-spin-slow" />
             </div>
         </main>
         <Footer />
